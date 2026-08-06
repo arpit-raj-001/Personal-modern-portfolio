47c938f7c1e70a858da99d6466ce39fa13895704
Arpit <arpit1206477417@gmail.com>
2026-08-07 05:05:00 +0530

    links

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -15,16 +15,17 @@ import {
 } from "react-icons/fi";
 import { SiLeetcode, SiCodeforces, SiCodechef } from "react-icons/si";
 import "./Navigation.css";
+import resumePdf from "./assets/arpit-raj-resume-101 (1) (1).pdf";
 
 const navItems = [
   { name: "Home", icon: <FiHome />, href: "#" },
-  { name: "Resume", icon: <FiBookOpen />, href: "#" },
+  { name: "Resume", icon: <FiBookOpen />, href: resumePdf, target: "_blank" },
   { name: "Projects", icon: <FiLayout />, href: "#" },
-  { name: "LinkedIn", icon: <FiLinkedin />, href: "#" },
-  { name: "LeetCode", icon: <SiLeetcode />, href: "#" },
-  { name: "Codeforces", icon: <SiCodeforces />, href: "#" },
-  { name: "CodeChef", icon: <SiCodechef />, href: "#" },
-  { name: "GitHub", icon: <FiGithub />, href: "#" },
+  { name: "LinkedIn", icon: <FiLinkedin />, href: "https://www.linkedin.com/in/arpit-raj-614240318", target: "_blank" },
+  { name: "LeetCode", icon: <SiLeetcode />, href: "https://leetcode.com/u/Arpit-raj/", target: "_blank" },
+  { name: "Codeforces", icon: <SiCodeforces />, href: "https://codeforces.com/profile/aadzz", target: "_blank" },
+  { name: "CodeChef", icon: <SiCodechef />, href: "https://www.codechef.com/users/arpit_9921", target: "_blank" },
+  { name: "GitHub", icon: <FiGithub />, href: "https://github.com/arpit-raj-001", target: "_blank" },
 ];
 
 export default function Navigation() {
@@ -86,6 +87,8 @@ function IconContainer({ item, mouseX }) {
   return (
     <a
       href={item.href}
+      target={item.target}
+      rel={item.target === "_blank" ? "noopener noreferrer" : undefined}
       className="nav-link-wrapper"
       style={{ display: "block", textDecoration: "none" }}
     >
